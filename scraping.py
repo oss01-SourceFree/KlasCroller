691dd64308fb5d74d39584222468cd37cc4b18a6
YunShin <2younsin@gmail.com>
2022-10-29 21:28:46 +0900

    cclass로 기능구현

--- scraping.py
+++ scraping.py
@@ -11,38 +11,62 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 
-url = "https://klas.kw.ac.kr/"
 
-'''
-창을 띄우지 않으려면 아래 # options.add_argument('headless') 코드의 주석을 해제하면 됩니다.
-'''
-options = webdriver.ChromeOptions()
-# options.add_argument('headless')
-
-browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
-
-# Klas 를 열고, 로그인을 수행하는 함수
-def accese_klas(id,pw):
-    # 1. chrome창을 띄어, klas로 이동 
-    browser.get(url)
-    # 2. id, pw 입력
-    browser.find_element(By.ID,"loginId").send_keys(id)
-    browser.find_element(By.ID,"loginPwd").send_keys(pw)
-    # 3. 로그인 버튼 클릭
-    browser.find_element(By.XPATH,"/html/body/div[1]/div/div/div[2]/form/div[2]/button").click()
+class Scraper:
+    def __init__(self):
+        self.url = "https://klas.kw.ac.kr/"
+        '''
+        창을 띄우지 않으려면 아래 # options.add_argument('headless') 코드의 주석을 해제하면 됩니다.
+        '''
+        options = webdriver.ChromeOptions()
+        # options.add_argument('headless')
+        self.browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
     
-    return browser
+    def accese_klas(self,id,pw):
+        # 1. chrome창을 띄어, klas로 이동 
+        self.browser.get(self.url)
+        # 2. id, pw 입력
+        self.browser.find_element(By.ID,"loginId").send_keys(id)
+        self.browser.find_element(By.ID,"loginPwd").send_keys(pw)
+        # 3. 로그인 버튼 클릭
+        self.browser.find_element(By.XPATH,"/html/body/div[1]/div/div/div[2]/form/div[2]/button").click()
 
-# 과목명을 가져오는 함수
-def scrape_subjectName(browser):
-    try:
-        elem = WebDriverWait(browser,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,"left")))
-        subject = list(set([name.text.split()[0] for name in elem]))
-        for str in subject:
-            if '-' in str:
-                sub_str = str.split('-') 
-                if sub_str[0].isdigit() and sub_str[1].isdigit():
-                    subject.remove(str)
-        return subject
-    except:
-        print('error!')
+
+
+
+
+# url = "https://klas.kw.ac.kr/"
+
+# '''
+# 창을 띄우지 않으려면 아래 # options.add_argument('headless') 코드의 주석을 해제하면 됩니다.
+# '''
+# options = webdriver.ChromeOptions()
+# # options.add_argument('headless')
+
+# browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
+
+# # Klas 를 열고, 로그인을 수행하는 함수
+# def accese_klas(id,pw):
+#     # 1. chrome창을 띄어, klas로 이동 
+#     browser.get(url)
+#     # 2. id, pw 입력
+#     browser.find_element(By.ID,"loginId").send_keys(id)
+#     browser.find_element(By.ID,"loginPwd").send_keys(pw)
+#     # 3. 로그인 버튼 클릭
+#     browser.find_element(By.XPATH,"/html/body/div[1]/div/div/div[2]/form/div[2]/button").click()
+    
+#     return browser
+
+# # 과목명을 가져오는 함수
+# def scrape_subjectName(browser):
+#     try:
+#         elem = WebDriverWait(browser,10).until(EC.presence_of_all_elements_located((By.CLASS_NAME,"left")))
+#         subject = list(set([name.text.split()[0] for name in elem]))
+#         for str in subject:
+#             if '-' in str:
+#                 sub_str = str.split('-') 
+#                 if sub_str[0].isdigit() and sub_str[1].isdigit():
+#                     subject.remove(str)
+#         return subject
+#     except:
+#         print('error!')
